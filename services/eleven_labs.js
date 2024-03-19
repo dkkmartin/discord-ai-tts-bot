7407aec1628ca066ffd3f2c8a4e242e6542eeee9
Martin <martinbruun1995@gmail.com>
2024-03-19 13:13:51 +0100

    Update config.json and eleven_labs.js

--- services/eleven_labs.js
+++ services/eleven_labs.js
@@ -1,10 +1,10 @@
-const axios = require("axios");
-const fs = require("node:fs");
-const {join} = require("node:path");
-const {pipeline} = require("node:stream/promises");
-const {v4} = require("uuid");
-const config = require("../cfg/config.json");
-const {error} = require("node:console");
+const axios = require('axios');
+const fs = require('node:fs');
+const { join } = require('node:path');
+const { pipeline } = require('node:stream/promises');
+const { v4 } = require('uuid');
+const config = require('../cfg/config.json');
+const { error } = require('node:console');
 
 module.exports = {
   generateAudio: async (text) => {
@@ -15,34 +15,34 @@ module.exports = {
     };
 
     const headers = {
-      Accept: "audio/mpeg",
-      "xi-api-key": process.env.EL_API_KEY,
-      "Content-Type": "application/json",
+      Accept: 'audio/mpeg',
+      'xi-api-key': process.env.EL_API_KEY,
+      'Content-Type': 'application/json',
     };
 
     const response = await axios
       .post(
         `https://api.elevenlabs.io/v1/text-to-speech/${config.eleven_labs.voice_id}/stream?optimize_streaming_latency=${config.eleven_labs.query_settings.optimize_streaming_latency}&output_format=${config.eleven_labs.query_settings.output_format}`,
         JSON.stringify(data),
-        {headers: headers, responseType: "stream"},
+        { headers: headers, responseType: 'stream' }
       )
       .catch((error) => {
-        throw error("Error on generating audio from Eleven Labs:", error);
+        throw error('Error on generating audio from Eleven Labs:', error);
       });
 
-    if (!response.data) throw error("Error in Eleven Labs API call");
+    if (!response.data) throw error('Error in Eleven Labs API call');
 
     const audio_id = v4();
 
-    const path = join(".", "output", `${audio_id}.mp3`);
+    const path = join('.', 'output', `${audio_id}.mp3`);
 
     const audio_path = await pipeline(response.data, fs.createWriteStream(path))
       .then(() => {
-        console.log("Audio saved: " + path);
+        console.log('Audio saved: ' + path);
         return path;
       })
       .catch((err) => {
-        throw error("Error in Pipeline while processing stream from Eleven Labs", err);
+        throw error('Error in Pipeline while processing stream from Eleven Labs', err);
       });
 
     return audio_path;
